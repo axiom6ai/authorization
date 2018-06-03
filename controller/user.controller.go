b00f23a17611824df58daddfc01708df83e6a0c5
Bo Dai <bo.dai@acuityads.com>
2018-06-03 01:30:06 -0400

    add user profile img upload and get

--- controller/user.controller.go
+++ controller/user.controller.go
@@ -3,8 +3,11 @@ package controller
 import (
 	"authorization/model"
 	"encoding/json"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strings"
 
 	"authorization/common"
@@ -23,6 +26,36 @@ type UserCtrl struct {
 func (ctrl UserCtrl) InitializeRoutes() {
 	ctrl.Router.HandleFunc("/api/login", ctrl.userLogin).Methods("POST")
 	ctrl.Router.HandleFunc("/api/signup", ctrl.createUser).Methods("POST")
+	ctrl.Router.HandleFunc("/api/profileImg", ctrl.getProfileImg).Methods("GET")
+	ctrl.Router.HandleFunc("/api/profileImg", ctrl.uploadProfileImg).Methods("POST")
+}
+
+func (ctrl UserCtrl) uploadProfileImg(w http.ResponseWriter, r *http.Request) {
+	file, handler, err := r.FormFile("profileImg")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer file.Close()
+	f, err := os.OpenFile("images/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+	io.Copy(f, file)
+}
+
+func (ctrl UserCtrl) getProfileImg(w http.ResponseWriter, r *http.Request) {
+	file, err := ioutil.ReadFile("images/download.jpeg")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Conent-type", "image/jpeg")
+	w.Write(file)
 }
 
 func (ctrl UserCtrl) getUser(user *model.User) (err error) {
