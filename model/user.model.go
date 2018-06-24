9fb3436c18d9e582a964dd847e5ce06593529e64
Bo Dai <bo.dai@acuityads.com>
2018-06-24 14:22:57 -0400

    update user model

--- model/user.model.go
+++ model/user.model.go
@@ -1,12 +1,15 @@
 package model
 
 type User struct {
-	ID          uint   `gorm:"primary_key" json:"id"`
-	Email       string `sql:"type: VARCHAR(255); not null; unique" json:"email"`
-	Password    string `sql:"type: VARCHAR(255); not null;" json:"password"`
-	ChineseName string `sql:"type: VARCHAR(255);" json:"chineseName"`
-	EnglishName string `sql:"type: VARCHAR(255);" json:"englishName"`
-	DateOfBirth string `sql:"type: VARCHAR(255);" json:"dateOfBirth"`
-	PhoneNumber string `sql:"type: VARCHAR(255);" json:"phoneNumber"`
-	PhotoPath   string `sql:"type: VARCHAR(255);" json:"photoPath"`
+	ID               uint   `gorm:"primary_key" json:"id"`
+	Email            string `sql:"type: VARCHAR(255); not null; unique" json:"email"`
+	Password         string `sql:"type: VARCHAR(255); not null;" json:"password"`
+	DateOfBirth      string `sql:"type: VARCHAR(255); not null" json:"dateOfBirth"`
+	FirstName        string `sql:"type: VARCHAR(255); not null" json:"firstName"`
+	LastName         string `sql:"type: VARCHAR(255); not null" json:"lastName"`
+	PhoneNumber      string `sql:"type: VARCHAR(255); not null" json:"phoneNumber"`
+	Gender           string `sql:"type: VARCHAR(255); not null" json:"gender"`
+	ChineseLastName  string `sql:"type: VARCHAR(255);" json:"chineseLastName"`
+	ChineseFirstName string `sql:"type: VARCHAR(255);" json:"chineseFirstName"`
+	PhotoPath        string `sql:"type: VARCHAR(255);" json:"photoPath"`
 }
